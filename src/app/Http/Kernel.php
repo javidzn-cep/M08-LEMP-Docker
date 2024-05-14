f865c69e75cc614fe880877f75fdaa2560c287c0
javidzn-cep <jdiazn2223@politecnics.barcelona>
2024-05-14 20:23:24 +0200

    cors

--- src/app/Http/Kernel.php
+++ src/app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\CorsMiddleware;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -15,6 +16,7 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
+        CorsMiddleware::class,
         \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
--- src/routes/api.php
+++ src/routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('/users', UsuariosController::class);
+Route::apiResource('/users', UsuariosController::class)->middleware('cors');
